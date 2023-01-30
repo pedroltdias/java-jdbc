f2974b5ad48ce6a20ece18514e1811007a38d6ff
Pedro Dias <pedroluksteixeira@gmail.com>
2023-01-30 10:25:20 -0300

    Feature: Desabilitating AutoCommit and Explicitating the Commit and Rollback

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -5,19 +5,32 @@ import java.sql.Statement;
 
 public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
-        String nome = "PRODUTO3";
-        String descricao = "DESCRICAO3";
-
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.recuperarConexao();
 
+        connection.setAutoCommit(false); //remove o poder de commit do jdbc e retoma para si(desenvolvedor)
+
         PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)");
 
+        try {
+            adicionarVariavel("PRODUTO7", "DESCRICAO7", stm);
+            adicionarVariavel("PRODUTO8", "DESCRICAO8", stm);
+
+            connection.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[ROOLBACK]");
+            connection.rollback();
+        } finally {
+            stm.close();
+            connection.close();
+        }
+    }
+
+    private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
 
         stm.execute();
-
-        connection.close();
     }
 }
